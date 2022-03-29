b827c6023a332f7df465ee184bbf7841ef1fa5be
ix5 <ix5@users.noreply.github.com>
2022-03-29 23:47:02 +0200

    isso: js: embed.js: Preserve default config values
    This way, Isso is not confusing users when a server config
    value overwrites a client default one, i.e. if the user
    has not actually attempted to overwrite it.

--- isso/js/app/config.js
+++ isso/js/app/config.js
@@ -1,28 +1,14 @@
 var utils = require("app/utils");
+var default_config = require("app/default_config");
 
 "use strict";
 
-var config = {
-    "css": true,
-    "css-url": null,
-    "lang": "",
-    "default-lang": "en",
-    "reply-to-self": false,
-    "require-email": false,
-    "require-author": false,
-    "reply-notifications": false,
-    "max-comments-top": "inf",
-    "max-comments-nested": 5,
-    "reveal-on-click": 5,
-    "gravatar": false,
-    "avatar": true,
-    "avatar-bg": "#f0f0f0",
-    "avatar-fg": ["#9abf88", "#5698c4", "#e279a3", "#9163b6",
-                  "#be5168", "#f19670", "#e4bf80", "#447c69"].join(" "),
-    "vote": true,
-    "vote-levels": null,
-    "feed": false
-};
+// Preserve default values to filter out when comparing
+// with values fetched from server
+var config = {};
+for (var key in default_config) {
+    config[key] = default_config[key];
+}
 
 var js = document.getElementsByTagName("script");
 
---
+++ isso/js/app/default_config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var default_config = {
+    "css": true,
+    "css-url": null,
+    "lang": "",
+    "default-lang": "en",
+    "max-comments-top": "inf",
+    "max-comments-nested": 5,
+    "reveal-on-click": 5,
+    "avatar": true,
+    "avatar-bg": "#f0f0f0",
+    "avatar-fg": ["#9abf88", "#5698c4", "#e279a3", "#9163b6",
+                  "#be5168", "#f19670", "#e4bf80", "#447c69"].join(" "),
+    "vote": true,
+    "vote-levels": null,
+    "feed": false
+};
+
+module.exports = default_config;
